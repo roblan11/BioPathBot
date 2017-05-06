270300b992839f0a2eb31e39a4a99523583c4d05
roblan11 <roblan11@hotmail.ch>
2017-05-06 21:10:45 +0200

    draw sea

--- map_test.py
+++ map_test.py
@@ -29,16 +29,18 @@ def findCorners(pts):
 # draws the map, some points and the lines
 def drawmap(pts, export=False):
     corners = findCorners(pts)
-    m1 = Basemap(llcrnrlon=corners[0]-1, llcrnrlat=corners[2]-1, urcrnrlon=corners[1]+1, urcrnrlat=corners[3]+1, resolution='i')
-    m1.drawcountries(linewidth=1.0, color='red')
+    m = Basemap(llcrnrlon=corners[0]-1, llcrnrlat=corners[2]-1, urcrnrlon=corners[1]+1, urcrnrlat=corners[3]+1, resolution='i')
+    m.drawmapboundary(fill_color='0.6')
+    m.drawcountries(linewidth=1.0, color='0.6')
+    m.fillcontinents(color='white', lake_color='white')
     for p in pts: # draw points
-        x, y = m1(p[0], p[1])
-        m1.plot(x, y, 'bo')
+        x, y = m(p[0], p[1])
+        m.plot(x, y, 'bo')
     for i in range(len(pts)-1): # draw lines
-        m1.plot([pts[i][0], pts[i+1][0]], [pts[i][1], pts[i+1][1]], color='blue');
+        m.plot([pts[i][0], pts[i+1][0]], [pts[i][1], pts[i+1][1]], color='blue');
     if export:
         plt.savefig('map1.png');
     plt.show()
 
-points = [[6, 46.5], [7, 47], [6, 47], [9, 46], [10, 48], [7.5, 47]]
-drawmap(points, True)
+points = [[6, 46.5], [7, 47], [6, 47], [9, 46], [10, 48], [30, 80]]
+drawmap(points, lines=False)
